a16b9858090b31ab4ddc3730114787f9cb5ffc52
leosanner <leosanner12@gmail.com>
2025-07-02 08:08:46 -0300

    Initial commit

---
+++ src/main/java/com/leonardosanner/programming_courses/ProgrammingCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.leonardosanner.programming_courses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammingCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingCoursesApplication.class, args);
+	}
+
+}
